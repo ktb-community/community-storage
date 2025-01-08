f83059d4966c6767ae993cc29c1bb27ac9936844
rivercity310 <h970126@gmail.com>
2025-01-08 15:13:54 +0900

    feat: CI/CD 키 변경 및 nginx 리로드

--- src/app.js
+++ src/app.js
@@ -3,7 +3,7 @@ const app = express();
 const {S3Client, GetObjectCommand} = require('@aws-sdk/client-s3');
 const multer = require('multer');
 const multerS3 = require('multer-s3');
-require('dotenv').config({ path: './.env' })
+require('dotenv').config({path: './.env'})
 
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
@@ -15,50 +15,41 @@ const REGION = process.env.REGION;
 const PORT = parseInt(process.env.PORT) || 8080;
 
 const s3Client = new S3Client({
-    region: REGION,
-    credentials: {
-        accessKeyId: ACCESS_KEY_ID,
-        secretAccessKey: SECRET_ACCESS_KEY
+    region: REGION, credentials: {
+        accessKeyId: ACCESS_KEY_ID, secretAccessKey: SECRET_ACCESS_KEY
     }
 });
 
 const storage = multerS3({
-    s3: s3Client,
-    bucket: BUCKET_NAME,
-    contentType: multerS3.AUTO_CONTENT_TYPE,
-    acl: 'public-read', // 버킷에서 acl 관련 설정을 풀어줘야 사용할 수 있다.
+    s3: s3Client, bucket: BUCKET_NAME, contentType: multerS3.AUTO_CONTENT_TYPE, acl: 'public-read', // 버킷에서 acl 관련 설정을 풀어줘야 사용할 수 있다.
     metadata: function (req, file, cb) {
         cb(null, {fieldName: file.fieldname});
-    },
-    key: function (req, file, cb) {
+    }, key: function (req, file, cb) {
         cb(null, `contents/${Date.now()}_${file.originalname}`);
     }
-})
+});
 
 const upload = multer({
     storage: storage
-})
+});
 
 app.get('/uploads/:key', async (req, res) => {
     const objectKey = req.params.key;
     const bucketName = BUCKET_NAME;
     const keyName = `contents/${objectKey}`;
 
-    const response = await s3Client.send(
-        new GetObjectCommand({
-            Bucket: bucketName,
-            Key: keyName,
-        }),
-    );
+    const response = await s3Client.send(new GetObjectCommand({
+        Bucket: bucketName, Key: keyName,
+    }),);
 
     return response.Body.pipe(res);
-})
+});
 
 app.post('/', upload.single('photo'), async (req, res) => {
     const photo = req.file;
     const loc = photo.location;
     const key = loc.substring(loc.lastIndexOf('/') + 1);
-    res.json({ key });
+    res.json({key});
 });
 
 app.listen(PORT || 8080, () => {
